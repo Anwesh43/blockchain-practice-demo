d764d745ab4fb6e401013d5b687a2038bb565a20
Anwesh43 <anweshthecool0@gmail.com>
2019-05-02 02:49:24 +0530

    using toString to get proper string

--- HashCreator.js
+++ HashCreator.js
@@ -1,2 +1,2 @@
 const SHA256 = require('crypto-js/sha256')
-module.exports = (obj) => SHA256(JSON.stringify(obj))
+module.exports = (obj) => SHA256(JSON.stringify(obj)).toString()
--- testHashCreator.js
+++ testHashCreator.js
@@ -10,7 +10,7 @@ const staticData = () => "this will remain static"
 const sha1 = createHash(staticData())
 const sha2 = createHash(staticData())
 
-assert(sha1.toString() === sha2.toString())
+assert(sha1 === sha2)
 console.log(`sha1 is ${sha1}`)
 console.log("*********************************************")
 console.log(`sha2 is ${sha2}`)
@@ -21,5 +21,5 @@ setTimeout(() => {
     console.log(`timedSha1 is ${timedSha1}`)
     console.log("*********************************************")
     console.log(`timedSha2 is ${timedSha2}`)
-    assert(timedSha1.toString() !== timedSha2.toString())
+    assert(timedSha1 !== timedSha2)
 }, 1001)
